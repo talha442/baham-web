e7a45320f4d1266827d002663f721abab9d758eb
talha faisal <57016608+talha442@users.noreply.github.com>
2023-05-31 22:32:02 +0500

    Created Rest Apis for User & Test Cases

--- baham/views.py
+++ baham/views.py
@@ -261,3 +261,140 @@ def delete_vehicle_model(request, uuid):
         return JsonResponse(response_data, status=200)
     else:
         return JsonResponse({'error': 'Invalid endpoint or method type'}, status=400)
+#####################
+#####USER PROFILE####
+#####################
+
+def get_all_user_profiles(request):
+    if request.method == 'GET':
+        users = UserProfile.objects.all().filter(voided=False)
+        print(users)
+        if users:
+            data = []
+            for user in users:
+                data.append({
+                    'uuid': user.uuid,
+                    'birthdate':user.birthdate,
+                    'gender':user.gender,
+                    'type':user.type,
+                    'address':user.address,
+                    'town':user.town,
+                    'primary_contact':user.primary_contact,
+                    'bio':user.bio,
+                    'date_created': user.date_created,
+                    'created_by': str(user.created_by),
+                })
+        else:
+            return JsonResponse({'ERROR': 'No User Profile Found'}, status=404)    
+        return JsonResponse({"results": data}, status=200)
+
+
+def get_user_profile(request, uuid):
+    if request.method == 'GET':
+        user_model = UserProfile.objects.filter(uuid=uuid).first()
+        if  user_model:
+            data = {
+                'uuid': user_model.uuid,
+                'birthdate':user_model.birthdate,
+                'gender':user_model.gender,
+                'type':user_model.type,
+                'address':user_model.address,
+                'town':user_model.town,
+                'primary_contact':user_model.primary_contact,
+                'bio':user_model.bio,
+                'date_created': user_model.date_created,
+                'created_by': str(user_model.created_by),
+            }
+            return JsonResponse({'results': data}, status=200)
+        else:
+            return JsonResponse({'ERROR': 'No User Profile Found'}, status=404)
+
+def create_user_profile(request):
+    if request.method == 'POST': 
+        _username = request.POST.get('username')
+        _pass = request.POST.get('pass')
+        _birthdate = request.POST.get('birthdate')
+        _gender = request.POST.get('gender')
+        _type = request.POST.get('type')
+        _primary_contact = request.POST.get('primary_contact')
+        _alternate_contact = request.POST.get('alternate_contact')
+        _address = request.POST.get('address')
+        _address_latitude = request.POST.get('address_latitude')
+        _address_longitude = request.POST.get('address_longitude')
+        _landmark = request.POST.get('landmark')
+        _town = request.POST.get('town')
+        _active = request.POST.get('active')
+        _bio = request.POST.get('bio')
+
+        user_profile = UserProfile.objects.create(
+            user = User.objects.create_user(username=_username, password=_pass),
+            birthdate=_birthdate,
+            gender=_gender,
+            type=_type,
+            primary_contact=_primary_contact,
+            alternate_contact=_alternate_contact,
+            address=_address,
+            address_latitude=_address_latitude,
+            address_longitude=_address_longitude,
+            landmark=_landmark,
+            town=_town,
+            active=_active,
+            bio=_bio,
+        )
+
+        response_data = {
+            'message': 'User profile created successfully',
+            'uuid': user_profile.uuid,
+        }
+        return JsonResponse(response_data, status=201)
+
+def update_user_profile(request, uuid):
+    if request.method == 'PUT':
+        params = QueryDict(request.body)
+        _type = params.get('type')
+        _primary_contact = params.get('primary_contact')
+        _alternate_contact = params.get('alternate_contact')
+        _address = params.get('address')
+        _address_latitude = params.get('address_latitude')
+        _address_longitude = params.get('address_longitude')
+        _landmark = params.get('landmark')
+        _town = params.get('town')
+        _active = params.get('active')
+        userprofile_model = UserProfile.objects.filter(uuid=uuid).first()
+        if not userprofile_model:
+            response_data = {
+                'ERROR': 'No User Profile Found'
+            }
+            return JsonResponse(response_data, status=404)
+        userprofile_model.type = _type
+        userprofile_model.primary_contact = _primary_contact
+        userprofile_model.alternate_contact = _alternate_contact
+        userprofile_model.address = _address
+        userprofile_model.address_latitude = _address_latitude
+        userprofile_model.address_longitude = _address_longitude
+        userprofile_model.landmark = _landmark
+        userprofile_model.town = _town
+        userprofile_model.active = _active
+        
+
+        userprofile_model.update()
+        response_data = {
+            'message': 'User Profile updated successfully',
+            'uuid': userprofile_model.uuid,
+        }
+        return JsonResponse({"results":response_data}, status=200)
+
+
+def delete_user_profile(request, uuid):
+    if request.method == 'DELETE':
+        user_model = UserProfile.objects.filter(uuid=uuid).first()
+        if not user_model:
+            response_data = {
+                'ERROR': 'No User Profile Found'
+            }
+            return JsonResponse(response_data, status=404)
+        user_model.delete()
+        response_data = {
+            'MESSAGE': 'User Profile deleted successfully'
+        }
+        return JsonResponse(response_data, status=200)
